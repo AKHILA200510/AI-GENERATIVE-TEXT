ec301c57747a4462145a18108995ec5e85860c2f
AKHILA200510 <prasadalapati744@gmail.com>
2026-03-31 11:15:38 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+# Configure Gemini
+genai.configure(api_key=os.getenv("api"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    user_input = request.json.get("message")
+
+    if not user_input:
+        return jsonify({"error": "Empty input"}), 400
+
+    try:
+        response = model.generate_content(user_input)
+        return jsonify({"response": response.text})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
