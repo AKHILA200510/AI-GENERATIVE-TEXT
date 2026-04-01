321b47eb16b9ffec6bdd22469ce2d210b96bc04d
AKHILA200510 <prasadalapati744@gmail.com>
2026-04-01 09:40:21 +0530

    updated backend and UI

--- app.py
+++ app.py
@@ -3,24 +3,30 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
 
-# Configure Gemini
-genai.configure(api_key="AIzaSyB5jzho3J04s82pAoZu2oJs4daG_rBbc40")
+# Configure Gemini API
+API_KEY = os.getenv("api")
+if not API_KEY:
+    raise ValueError("API key not found. Set GEMINI_API_KEY in .env")
+
+genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/generate", methods=["POST"])
 def generate():
-    data=request.get_json()
-    user_input=data.get("message")if data else None
-    print("INPUT:",user_input)
+    data = request.get_json()
+    user_input = data.get("message") if data else None
 
     if not user_input:
         return jsonify({"error": "Empty input"}), 400
@@ -31,3 +37,7 @@ def generate():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+# Render requires this
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
